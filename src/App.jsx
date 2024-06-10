011fd470bd7ab0e774be79d6a9fa01ce925b19b4
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-06-10 10:24:15 -0300

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import styled from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import backgroundImage from '/imagens/banner.png'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
-  
-` 
+`
+const AppContainer = styled.div`
+width: 1440px;
+max-width: 100%;
+margin: 0 auto;
+`
+const MainContainer = styled.main`
+display: flex;
+gap: 24px;
+`
 
 function App() {
-
-
   return (
     <FundoGradiente>
-      <EstilosGlobais/>
-      <Cabecalho/>
+      <EstilosGlobais />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+        <BarraLateral />
+        <Banner
+          texto="A galeria mais completa de fotos do espaço!"
+          backgroundImage={backgroundImage}
+        />
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
   )
 }
--- src/componentes/EstilosGlobais/index.jsx
+++ src/componentes/EstilosGlobais/index.jsx
@@ -1,9 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
+import GandhiSansBold from "./Fontes/GandhiSans-Bold.otf"
+import GandhiSansRegular from "./Fontes/GandhiSans-Regular.otf"
+
 const EstilosGlobais = createGlobalStyle`
+@font-face {
+  font-family: 'GandhiSansRegular';
+  src: local('Gandhi Sans Regular'), local("GandhiSansRegular"), url(${GandhiSansRegular})
+}
+@font-face {
+  font-family: 'GandhiSansBold';
+  src: local('Gandhi Sans Bold'), local("GandhiSansBold"), url(${GandhiSansBold})
+}
 html {
 line-height: 1.15; 
 -webkit-text-size-adjust: 100%; 
+font-family: GandhiSansRegular;
 }
 body {
   margin: 0;
