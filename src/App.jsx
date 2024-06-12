e7da12f7670d6a8757c63457e296b4c9e3d806c4
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-06-11 21:05:07 -0300

    modal zoom

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import backgroundImage from '/imagens/banner.png'
 import Galeria from "./componentes/Galeria"
 import fotos from "./fotos.json"
 import { useState } from "react"
+import ModalZoom from "./componentes/ModalZoom"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -32,7 +33,7 @@ const ConteudoGaleria = styled.section`
 const App = () => {
 
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
-
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -45,10 +46,16 @@ const App = () => {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={backgroundImage}
             />
-            <Galeria fotos={fotosDaGaleria}/>
+            <Galeria 
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)}
+              fotos={fotosDaGaleria}/>
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <ModalZoom 
+        foto={fotoSelecionada}
+        aoFechar={() => setFotoSelecionada(null)}
+      />
     </FundoGradiente>
   
   )
---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components"
+import Imagem from "../Galeria/Imagem"
+import BotaoIcone from "../BotaoIcone/Index"
+
+const Overley = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+    background: transparent;
+    padding: 0;
+    border: 0;
+    width: 1156px;
+    display: flex;
+    justify-content: center;
+    form {
+        button: {
+            position: relative;
+            top: 20x;
+            right: 60px;
+        }
+
+     }
+    
+    
+`
+
+
+const ModalZoom = ({ foto, aoFechar }) => {
+    return (
+        <>
+            {foto && <>
+                <Overley />
+                <DialogEstilizado open={!!foto} onClose={aoFechar}>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <BotaoIcone method="dialog">
+                        <img src="/icones/fechar.png" alt="Icone de fechar" />
+                        </BotaoIcone>
+                    </form>
+                </DialogEstilizado>
+            </>}
+        </>
+    )
+}
+
+export default ModalZoom
